f6fecfca203288660240e2a5c0ea97e9099f34dc
sooyeon-kr <sooyeon7027@naver.com>
2024-05-25 15:15:18 +0900

    update : server.js - sequelize 설정 추가

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const morgan = require('morgan');
 
+// index.js에 있는 db.sequelize 객체 모듈을 구조분해로 불러온다.
+const { sequelize } = require('./models/index');
 const app = express();
+
+sequelize
+    .sync({ force: false })
+    .then(() => {
+        console.log('데이터베이스 연결됨.');
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+app.use(morgan('dev')); // 로그
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World!" });
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello World!' });
 });
 
 // 포트넘버 설정
 app.listen(3000, () => {
-  console.log("Server is running on port 3000.");
+    console.log('Server is running on port 3000.');
 });
