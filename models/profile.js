19c042089d2629082af72cc619821536c41dd2e0
Ho-Da-Dak <godqhr721@gmail.com>
2024-05-27 15:42:13 +0900

    feat : profile.js,index.js - Profile 모델 생성 및 연결

---
+++ models/profile.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define(
+        'Profile',
+        {
+            profile_id: {
+                type: DataTypes.STRING(30),
+                primaryKey: true,
+                comment: '사용자 ID',
+                references: {
+                    model: 'users',
+                    key: '_id',
+                },
+            },
+            portfolio_url: {
+                type: DataTypes.STRING,
+                comment: '포트폴리오 링크',
+            },
+            introduction: {
+                type: DataTypes.STRING,
+                comment: '자기소개',
+            },
+            image_url: {
+                type: DataTypes.STRING,
+                comment: '프로필 사진 URL',
+            },
+        },
+        {
+            timestamps: true,
+        }
+    );
+};
