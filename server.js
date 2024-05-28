adba4d69f41eb08542650623dc40e769e6e68f1a
sooyeon-kr <sooyeon7027@naver.com>
2024-05-28 11:00:14 +0900

    feat : server.js, package, gitignore - redis 추가 및 설정

--- server.js
+++ server.js
@@ -5,21 +5,25 @@ const swaggerFile = require('./swagger/swagger-output');
 const cors = require('cors');
 const path = require('path');
 const morgan = require('morgan');
-
 // index.js에 있는 db.sequelize 객체 모듈을 구조분해로 불러온다.
 const app = express();
+require('dotenv').config();
+
 app.use(cors()); // temp
 
 app.use(morgan('dev')); // 로그
 
 // users 라우트 모듈 가져오기
 const usersRouter = require('./routes/users');
+const gardensRouter = require('./routes/gardens');
+app.use(express.json());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // users 라우트를 '/users' 경로에 연결
 app.use('/users', usersRouter);
+app.use('/gardens', gardensRouter);
 app.get('/', (req, res) => {
     res.json({ message: 'Hello World!' });
 });
