2d314e370edecc96765691992f8aa90a7285baef
sooyeon-kr <sooyeon7027@naver.com>
2024-05-28 19:59:59 +0900

    feat : socketHandler.js - roomId 특정 방에 소켓 연결하는 joinRoom 기능 구현

--- routes/socketHandler.js
+++ routes/socketHandler.js
@@ -10,12 +10,19 @@ module.exports = (server) => {
         console.log('a user connected');
 
         // 유저가 룸에 입장하겠다.라고 joinRoom 이벤트를 발생시키면
-        socket.on('joinRoom', (roomId) => {
+        socket.on('joinRoom', ({ roomId, userId }) => {
             // 현재 소켓을 roomId에 해당하는 방에 참여시킴
             socket.join(roomId);
+
+            console.log('joinRoom 이벤트 실행 at server');
             console.log(`User joined room: ${roomId}`);
+
+            console.log('소켓 연결 수 확인');
+            const room = io.sockets.adapter.rooms.get(roomId);
+            const numClients = room ? room.size : 0;
+            console.log(`Number of clients in room ${roomId}: ${numClients}`);
             // roodId에 해당하는 방에 있는 모든 소켓에게 이벤트 userJoined라는 이벤트 전송
-            socket.to(roomId).emit('userJoined', socket.id);
+            socket.to(roomId).emit('userJoined', userId);
         });
 
         socket.on('disconnect', () => {
