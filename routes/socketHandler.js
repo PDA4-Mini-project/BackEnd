576d5899f122efb1d7e5ec7d763d5dc40e1c450c
sooyeon-kr <sooyeon7027@naver.com>
2024-05-30 01:24:58 +0900

    임시커밋

--- routes/socketHandler.js
+++ routes/socketHandler.js
@@ -3,44 +3,47 @@ const socketIo = require('socket.io');
 module.exports = (server) => {
     const io = socketIo(server, {
         cors: {
-            origin: '*',
+            origin: '*', // 실제 배포시에는 보다 구체적인 호스트 지정이 필요
         },
     });
+
     io.on('connection', (socket) => {
-        console.log('a user connected');
+        console.log('A user connected:', socket.id);
 
         socket.on('candidate', ({ roomId, candidate }) => {
-            console.log('candidate:', candidate);
+            console.log('Candidate received:', candidate);
             socket.to(roomId).emit('candidate', { candidate });
         });
 
         socket.on('offer', ({ roomId, offer }) => {
-            console.log('offer: ', offer);
+            console.log('Offer received:', offer);
             socket.to(roomId).emit('offer', offer);
         });
+
         socket.on('answer', ({ roomId, answer }) => {
-            console.log('answer: ', answer);
+            console.log('Answer received:', answer);
             socket.to(roomId).emit('answer', answer);
         });
 
-        // 유저가 룸에 입장하겠다.라고 joinRoom 이벤트를 발생시키면
         socket.on('joinRoom', ({ roomId, userId }) => {
-            // 현재 소켓을 roomId에 해당하는 방에 참여시킴
             socket.join(roomId);
+            console.log('User joined room:', roomId);
+            console.log(`User ID: ${userId}`);
 
-            console.log('joinRoom 이벤트 실행 at server');
-            console.log(`User joined room: ${roomId}`);
-
-            console.log('소켓 연결 수 확인');
+            // 방에 있는 사용자 수 확인
             const room = io.sockets.adapter.rooms.get(roomId);
             const numClients = room ? room.size : 0;
             console.log(`Number of clients in room ${roomId}: ${numClients}`);
-            // roodId에 해당하는 방에 있는 모든 소켓에게 이벤트 userJoined라는 이벤트 전송
-            io.to(roomId).emit('userJoined', userId);
-        });
 
-        socket.on('disconnect', () => {
-            console.log('user disconnected');
+            // 방에 새로운 사용자가 참여했다는 것을 방의 모든 사용자에게 알림
+            socket.to(roomId).emit('userJoined', { userId, numClients });
+
+            // 사용자가 방을 떠날 때 처리
+            socket.on('disconnect', () => {
+                console.log('User disconnected:', socket.id);
+                socket.to(roomId).emit('userLeft', userId);
+                socket.leave(roomId);
+            });
         });
     });
 };
