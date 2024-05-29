905a3bd298bb107a8b6aec098ea52046777b29be
sooyeon-kr <sooyeon7027@naver.com>
2024-05-29 09:58:19 +0900

    test : index.html - webRTC test를 위한 index.html 업데이트

--- routes/socketHandler.js
+++ routes/socketHandler.js
@@ -9,6 +9,20 @@ module.exports = (server) => {
     io.on('connection', (socket) => {
         console.log('a user connected');
 
+        socket.on('candidate', ({ roomId, candidate }) => {
+            console.log('candidate:', candidate);
+            socket.to(roomId).emit('candidate', { candidate });
+        });
+
+        socket.on('offer', ({ roomId, offer }) => {
+            console.log('offer: ', offer);
+            socket.to(roomId).emit('offer', offer);
+        });
+        socket.on('answer', ({ roomId, answer }) => {
+            console.log('answer: ', answer);
+            socket.to(roomId).emit('answer', answer);
+        });
+
         // 유저가 룸에 입장하겠다.라고 joinRoom 이벤트를 발생시키면
         socket.on('joinRoom', ({ roomId, userId }) => {
             // 현재 소켓을 roomId에 해당하는 방에 참여시킴
@@ -22,7 +36,7 @@ module.exports = (server) => {
             const numClients = room ? room.size : 0;
             console.log(`Number of clients in room ${roomId}: ${numClients}`);
             // roodId에 해당하는 방에 있는 모든 소켓에게 이벤트 userJoined라는 이벤트 전송
-            socket.to(roomId).emit('userJoined', userId);
+            io.to(roomId).emit('userJoined', userId);
         });
 
         socket.on('disconnect', () => {
